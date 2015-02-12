f8cdb08c506c2f49893c4721a481eeb614193093
David Rothblatt <daroth13@gmail.com>
2015-02-11 19:01:52 -0500

    first attempts at cracking Knight's Tour

--- KnightsTour.java
+++ KnightsTour.java
@@ -1,5 +1,3 @@
-
-
 import java.util.*;
 import java.io.*;
 
@@ -31,7 +29,14 @@ public class KnightsTour{
     public String toString(){
 	String ans = "\n";
 	//build your knights tour here...
-	return hide + go(0,0) + ans + "\n" + show;
+	for (int i = 0; i < board.length; i++){
+	    for (int j = 0; j < board.length; j++){
+		ans += board[i][j] + " ";
+	    }
+	    ans += "\n";
+	}
+	return ans; 
+	//return hide + go(0,0) + ans + "\n" + show;
     }
 
     public KnightsTour(int size){
@@ -44,12 +49,12 @@ public class KnightsTour{
     }
 
     public void solve(){ // same algo both ways 
-	
+	solve(0,0);
     }
 
 
     public void solve(int startx, int starty){ // same algo both ways 
-				
+	solve(startx, starty, 0);			
     }
 
 
@@ -58,9 +63,45 @@ public class KnightsTour{
     public boolean solve(int x,int y,int currentMoveNumber){
 	System.out.println(this); // take out later
 	wait(20); // take out later
-				
+	
+	if ( x < 0 || x >= board.length || y < 0
+	     || y >= board[0].length) {
+	    return false;
+	
+	}
+
+	if ( board[x][y] == 0 && currentMoveNumber != 0) {
+	    return true;
+	}
+	
+	if ( board[x][y] == -1 ) {
+	    board[x][y] = currentMoveNumber;
+	    if (solve(x+1,y+2,currentMoveNumber++) ||
+		    solve(x+1,y-2,currentMoveNumber++) ||
+		    solve(x-1,y+2,currentMoveNumber++) ||
+		    solve(x-1,y-2,currentMoveNumber++) ||
+		    solve(x+2,y+1,currentMoveNumber++) ||
+		    solve(x+2,y-1,currentMoveNumber++) ||
+		    solve(x-2,y+1,currentMoveNumber++) ||
+		    solve(x-2,y-1,currentMoveNumber++)
+		){
+		return true;
+	    }
+	    board[x][y] = -1;
+	}
 	return false;
     }
 
-
+    public static void main(String[]args){
+ 	KnightsTour kt1 = new KnightsTour(1);
+	//System.out.println(kt1.solve(0,0,0));
+	KnightsTour kt2 = new KnightsTour(2);
+	//System.out.println(kt2.solve(0,0,0));
+	KnightsTour kt3 = new KnightsTour(3);
+	//System.out.println(kt3.solve(0,0,0));
+	KnightsTour kt5 = new KnightsTour(5);
+	System.out.println(kt5.solve(0,0,0));
+	KnightsTour kt8 = new KnightsTour(8);
+	System.out.println(kt8 /* kt8.solve(0,0,0) */);
+    }
 }
