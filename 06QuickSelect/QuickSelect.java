1431c7f0f6535eda8f1099b71d4e1a887e36f143
drothblatt <daroth13@gmail.com>
2015-03-05 15:33:38 -0500

    more updates via iPad

--- 06QuickSelect/QuickSelect.java
+++ 06QuickSelect/QuickSelect.java
@@ -1,28 +1,33 @@
+import java.util.*;
+
 public class QuickSelect{
     
     public static void partition(int[] ary, int start, int end){
+        System.out.println(Arrays.toString(ary));
 	int[] D = new int[ary.length];
+        for (int i = 0; i < start; i++) {
+            D[i] = ary[i];
 	for (int i = end+1; i < D.length; i++){
 	    D[i] = ary[i];
 	}
+        System.out.println(Arrays.toString(D));
 
 	int pivot = ary[start];
-         
         start++;
 
 
         while ( start < end ) { 
              if ( ary[start] < pivot ) { 
                   D[start] = ary[start];
+                  start++;
              } else {
-                  D[start] = ary[start];
+                  D[end] = ary[start];
+                  start--;
              }
         }
+        D[start] = pivot;
         
-
-	
-
-
+        System.out.println(Arrays.sort(D));
 
     }
 
