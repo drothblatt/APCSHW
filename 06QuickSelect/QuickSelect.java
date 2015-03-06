d4c901e7c63ab6a09ac24dfde187c4ac4d0c29b7
David Rothblatt <daroth13@gmail.com>
2015-03-05 22:24:50 -0500

    parition() is finito.

--- 06QuickSelect/QuickSelect.java
+++ 06QuickSelect/QuickSelect.java
@@ -14,19 +14,23 @@ public class QuickSelect{
         System.out.println(Arrays.toString(D));
 
 	int pivot = ary[start];
-	start++;
-	
+	//System.out.println("pivot: " + pivot);
 	int pos = start;
-        while ( start < end ) { 
-             if ( ary[pos] < pivot ) { 
-                  D[start] = ary[pos];
-                  start++;
-             } else {
-                  D[end] = ary[pos];
-                  end--;
-             }
-	     pos++;
+	//System.out.println("While Loop:");
+        while ( start < end) { 
+	    if ( ary[pos] < pivot ) { 
+		//System.out.println ( ary[pos] + " < " + pivot) ;
+		D[start] = ary[pos];
+		start++;
+	    } else if ( ary[pos] > pivot ) {
+		//System.out.println ( ary[pos] + " > " + pivot) ;
+		D[end] = ary[pos];
+		end--;
+	    }
+	    pos++;
+	    System.out.println(Arrays.toString(D));
         }
+	//System.out.println( start + " == " + end + " ?");
 	
 	if (start == end){
 	    D[start] = pivot;
@@ -36,8 +40,8 @@ public class QuickSelect{
     }
 
     public static void main(String[]args){
-	int[] testing = {21, 7, 14, 18, 2, 13, 10, 6, 17};
-	partition(testing, 2, 7);
+	int[] testing = {21, 7, 19, 1, 4, 3, 23, 6, 35, 17};
+	partition(testing, 4, 8);
     }
 
     // yello.
