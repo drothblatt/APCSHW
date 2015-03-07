b99d1e237a01b2111258c7e1b0544c877b12ad66
David Rothblatt <daroth13@gmail.com>
2015-03-07 18:05:31 -0500

    beginnings of the quickSelect method made

--- 06QuickSelect/QuickSelect.java
+++ 06QuickSelect/QuickSelect.java
@@ -2,45 +2,61 @@ import java.util.*;
 
 public class QuickSelect{
 
+    public static int quickSelect(int[] ary, int k){
+	if (k <= 0 || k > ary.length){
+	    throw new IndexOutOfBoundsException();
+	}
+	return 1;
+    }
+
 
     public static int partition(int[] ary, int start, int end){
         System.out.println(Arrays.toString(ary));
 	int[] d = new int[ary.length];
-        for (int i = 0; i < start; i++) {
-            d[i] = ary[i];
-	}
-	for (int i = end+1; i < d.length; i++){
-	    d[i] = ary[i];
-	}
+
         //System.out.println(Arrays.toString(d));
 	Random r = new Random();
 	int pivot = ary[start + r.nextInt(end - start + 1) ];
+
 	System.out.println("pivot: " + pivot + " , start: " + start + 
 			   " , end: " + end);
 	int pos = start;
+	int temp = 0;
 	//System.out.println("While Loop:");
         while ( start < end) { 
 	    if ( ary[pos] < pivot ) { 
-		//System.out.println ( ary[pos] + " < " + pivot) ;
+		System.out.println ( ary[pos] + " < " + pivot) ;
 		d[start] = ary[pos];
 		start++;
 	    } else if ( ary[pos] > pivot ) {
-		//System.out.println ( ary[pos] + " > " + pivot) ;
+		System.out.println ( ary[pos] + " > " + pivot) ;
 		d[end] = ary[pos];
 		end--;
 	    }
 	    pos++;
-	    //System.out.println(Arrays.toString(d));
+	    System.out.println(Arrays.toString(ary));
         }
 	//System.out.println( start + " == " + end + " ?");
 
 	d[start] = pivot; // no if statement needed
-
         System.out.println(Arrays.toString(d));
+	return start;
+
+    }
+
+    public static void main(String[]args){
+	int[] testing = {21, 7, 19, 1, 4, 3, 23, 6, 35, 17};
+	//System.out.println(partition(testing, 2, 7));
+	System.out.println(quickSelect(testing,5)); 
+	System.out.println(quickSelect(testing,-3)); // returns exception as it should
+	System.out.println(quickSelect(testing,15)); // returns exception as it should
+	
+	
+
     }
 
     
-    // fin.
+    
 }
 
     
