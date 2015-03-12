8c60e20aeee7e64c5437e0dbde6e64df45e09138
David Rothblatt <daroth13@gmail.com>
2015-03-11 21:24:47 -0400

    beginnings of LinkedList Hw

--- 07LinkedList/LNode.java
+++ 07LinkedList/LNode.java
@@ -1,6 +1,24 @@
 public class LNode{
     private int data; //or whatever type the data is
     private LNode next; //LNode in an LNode… this is the reference to the “cdr”
+    
     //get and set Data
+    public int getData(){
+	return data;
+    }
+    public void setData(int x){
+	data = x;
+    }
+
     //get and set Next
+    public int getNext(){
+	return next;
+    }
+
+    public void setNext(LNode y){
+	next = y;
+    }
+
+    // throwback to beginning of the year w/ these accesor/mutator methods?
+
 }
