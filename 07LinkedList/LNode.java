0da39d3ecbf797245624624a542e0a1576b4f76b
David Rothblatt <daroth13@gmail.com>
2015-03-11 21:20:56 -0400

    beginnings of LinkedList Hw

---
+++ 07LinkedList/LNode.java
@@ -0,0 +1,6 @@
+public class LNode{
+    private int data; //or whatever type the data is
+    private LNode next; //LNode in an LNode… this is the reference to the “cdr”
+    //get and set Data
+    //get and set Next
+}
