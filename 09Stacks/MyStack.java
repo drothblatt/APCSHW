59817a3b661340b7d6a32d791cca0a13440213fb
David Rothblatt <daroth13@gmail.com>
2015-03-21 17:04:44 -0400

    name and empty method

---
+++ 09Stacks/MyStack.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class MyStack{
+    private MyLinkedList<T> l;
+
+    public String name(){
+	return "rothblatt.david";
+    }
+
+    public boolean empty(){
+	return (l.size == 0);
+    }
+
+
+}
