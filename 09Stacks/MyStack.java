b561b4a081a6b87124a63920371b26dcdad4b256
David Rothblatt <daroth13@gmail.com>
2015-03-21 20:44:52 -0400

    added push and peek. using LinkedList<T> over MyLinkedList <T>

--- 09Stacks/MyStack.java
+++ 09Stacks/MyStack.java
@@ -1,15 +1,25 @@
 import java.util.*;
 
-public class MyStack{
-    private MyLinkedList<T> l;
+public class MyStack<T>{
+    private LinkedList<T> l;
 
     public String name(){
 	return "rothblatt.david";
     }
 
     public boolean empty(){
-	return (l.size == 0);
+	return (l.size() == 0);
     }
 
+    public T push(T value){
+	l.add(0,value);
+	return value;
+    }
+
+    public T peek(){
+	return l.get(0);
+    }
+
+    
 
 }
