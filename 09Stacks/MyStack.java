5197a46159118b547b634c6370a0328158303207
David Rothblatt <daroth13@gmail.com>
2015-03-22 00:41:26 -0400

    added code to throw errors in case it's empty. code is complete.

--- 09Stacks/MyStack.java
+++ 09Stacks/MyStack.java
@@ -21,10 +21,12 @@ public class MyStack<T>{
     }
 
     public T peek(){
+	if (empty()) throw new EmptyStackException();
 	return l.get(0);
     }
 
     public T pop(){
+	if (empty()) throw new EmptyStackException();
 	return l.remove(0);
     }
 
@@ -32,7 +34,10 @@ public class MyStack<T>{
 
     public static void main(String[]args){
 	MyStack<Integer> s = new MyStack<Integer>(); 
-	System.out.println("\n\nMyStack\n");
+	MyStack<String> t = new MyStack<String>();
+
+	System.out.println("\n\nMyStack...Integer\n");
+	System.out.println("expecting true:  " + s.empty() );
 	s.push(1);
 	s.push(1);
 	s.push(2);
@@ -44,7 +49,30 @@ public class MyStack<T>{
 	System.out.println("expecting [3, 2, 1, 1]:       " + s.toString() );
 	s.push(8);
 	System.out.println("expecting [8, 3, 2, 1, 1]:    " + s.toString() );
+	System.out.println("expecting false:  " + s.empty() );
 	System.out.println("\n\n");
+
+	System.out.println("\n\nMyStack... String\n");
+	System.out.println("expecting true:  " + t.empty() );
+	t.push("Howdy");
+	t.push("1");
+	t.push("-_-");
+	t.push("^o^");
+	t.push("o_o");
+	System.out.println(t.toString() );
+	System.out.println(t.peek()); // should print out 5
+	System.out.println(t.pop()); // should print out 5 and remove it 
+	System.out.println(t.toString() );
+	t.push("8");
+	System.out.println(t.toString() );
+	System.out.println(t.empty() );
+	System.out.println("\n\n");
+	while (! t.empty() ){
+	    t.pop();
+	}
+	System.out.println(t.toString() );
+	t.pop();
+
     }
 
 }
