8a99a0fef01f1b2b96ec817777fe40af128a094f
David Rothblatt <daroth13@gmail.com>
2015-03-23 19:05:45 -0400

    tested. done.

--- 09StackAndQueue/MyQueue.java
+++ 09StackAndQueue/MyQueue.java
@@ -18,6 +18,24 @@ public class MyQueue<T>{
 
     public T dequeue(){
 	return q.remove(0); // will use remove method in myLL
+    }   
+    
+    public static void main(String[]args){
+	/*
+
+	MyQueue<String> kew = new MyQueue<String>();
+	System.out.println(kew.enqueue(".") + "\n"
+			   + kew.enqueue("_") + "\n"
+			   + kew.enqueue(",") + "\n"
+			   + kew.enqueue("/") + "\n"
+			   + kew.toString()  );
+	// expect [ . _ , / ]
+	System.out.println(kew.dequeue() + "\n" + kew.toString() + 
+			   "\n" + kew.dequeue() + "\n" + kew.dequeue() + "\n"
+			   + kew.toString()    );
+	// FIFO so expect "." then [ _ , / ] then "_" then "," then [ / ]
+
+	*/
     }
 
 }
