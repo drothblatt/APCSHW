e2a46247ff0a8422ecb72c96d16d370ebf2f8aa1
David Rothblatt <daroth13@gmail.com>
2015-03-25 12:10:06 -0400

    constructors added.all methods are set up.

---
+++ 10DEQueue/MyDeque.java
@@ -0,0 +1,49 @@
+import java.util.*;
+
+public class MyDeque{
+    private int head, tail, size;
+    private Object[] data;
+
+    public MyDeque(){
+	this(100);
+    }
+    public MyDeque(int s){
+	data = new Object[s];
+	head = s/2-1;
+	tail = s/2-1;
+	size = 0;
+    }
+
+    public String name(){
+	return "rothblatt.david";
+    }
+
+    public void addFirst(T value){
+
+
+    }
+
+    public void addLast(T value){
+
+
+    }
+
+    public T removeFirst(){
+
+
+    }
+
+    public T removeLast(){
+
+
+    }
+
+    public T getFirst(){
+    }
+
+    public T getLast(){
+
+    }
+
+
+}
