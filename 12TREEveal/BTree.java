b2df6b5c8c17dfb6db133472cb4762703bc686d0
David Rothblatt <daroth13@gmail.com>
2015-04-22 12:23:45 -0400

    Beginning of Garbage Binary Tree

---
+++ 12TREEveal/BTree.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class BTree<T>{
+    TreeNode<T> root = new TreeNode<T>();
+
+    public void add(T value){
+
+
+    }
+
+    public void add(TreeNode root, T value){
+
+    }
+
+}
---
+++ 12TREEveal/TreeNode.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+
+public class TreeNode<T>{
+	T value;
+	TreeNode parent, left, right;
+
+	public TreeNode(){
+		parent = null; 
+		left = null;
+		right = null;
+	} 
+
+
+}
