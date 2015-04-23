bf231a576589c6c64aacb80870523a4b28d08e6a
David Rothblatt <daroth13@gmail.com>
2015-04-23 12:02:15 -0400

    TreeNode class established with all the proper methods as well as some possibly helpful constructors

---
+++ 12TREEveal/.#BTree.java
@@ -0,0 +1,1 @@
+david.rothblatt@cslab4-14.stuy.edu.17600:1429619534
--- 12TREEveal/TreeNode.java
+++ 12TREEveal/TreeNode.java
@@ -1,15 +1,48 @@
 import java.util.*;
 import java.io.*;
 
-public class TreeNode<T>{
-	T value;
-	TreeNode parent, left, right;
+public class TreeNode<E>{
+	E value;
+	TreeNode left, right;
 
-	public TreeNode(){
-		parent = null; 
-		left = null;
-		right = null;
+	public TreeNode(E someVal){
+	    value = someVal;
 	} 
+	
+	public TreeNode(E someVal, TreeNode<E> nodeL, TreeNode<E> nodeR){
+	    value = someVal;
+	    left = nodeL;
+	    right = nodeR;
+	}
 
+	public int getLeft(){
+	    return left;
+	}
+	
+	public int getRight(){
+	    return right;
+	}
+
+	public int getValue(){
+	    return value;
+	}
+
+	public void setLeft(TreeNode<E> newLeft){
+	    left = newLeft;
+	}
+
+	public void setRight(TreeNode<E> newRight){
+	    right = newRight;
+	}
+
+	public void setValue(E someVal){
+	    value = someVal;
+	}
+
+	public String toString(){
+	    return "My Value: " + getValue() 
+		+ "Left Node: " + getLeft() + 
+		"Right Node: " + getRight() ;
+	}
 
 }
