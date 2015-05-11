755889094923e8adb8f815a6606e77344ece6ebb
David Rothblatt <daroth13@gmail.com>
2015-05-11 19:03:20 -0400

    quick beginning to the MyHeap.java

--- 13HeapingPileOfCookies/MyHeap.java
+++ 13HeapingPileOfCookies/MyHeap.java
@@ -2,25 +2,29 @@ import java.util.*;
 import java.io.*;
 
 public class MyHeap{
-    
+    private int[] heapArr;
+    private int size;
 
     public MyHeap(){  // creates a max heap
-
-
+	heapArr = new int[10];
+	size = 0;
     }
     
     public String toString() {
-
-
+	String retStr = "";
+	for (int i = 1; i <= size; i++){}
+	return retStr;
     }
     
     public int remove(){ // remove the root and return it
-
-
+	int removed = heapArr[1];
+	heapArr[1] = heapArr[size];
+	size--;
+	return removed;
     }
 
-    public void add(int) {  // add the int to the heap
-
+    public void add(int x) {  // add the int to the heap
+	size++;
     }
 
     public String name(){
