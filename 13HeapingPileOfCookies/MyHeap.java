008ef4149fc6c8f4f403f07807121b682ced7739
David Rothblatt <daroth13@gmail.com>
2015-05-11 18:55:51 -0400

    initial commit for MyHeap.java

---
+++ 13HeapingPileOfCookies/MyHeap.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class MyHeap{
+    
+
+    public MyHeap(){  // creates a max heap
+
+
+    }
+    
+    public String toString() {
+
+
+    }
+    
+    public int remove(){ // remove the root and return it
+
+
+    }
+
+    public void add(int) {  // add the int to the heap
+
+    }
+
+    public String name(){
+	return "rothblatt.david";
+    }
+
+
+}
