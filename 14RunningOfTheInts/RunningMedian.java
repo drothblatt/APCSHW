30f5cddeb9b812c3f08e569e4e7c5e5ed0ced4e7
David Rothblatt <daroth13@gmail.com>
2015-05-14 12:21:18 -0400

    worked on getMedian()

--- 14RunningOfTheInts/RunningMedian.java
+++ 14RunningOfTheInts/RunningMedian.java
@@ -9,14 +9,25 @@ public class RunningMedian{
     }
 
     public void add(int value){
-
-
+	if (maxH.size() == minH.size() || maxH.size() == minH.size() - 1){
+	    maxH.add(value);
+	} else if(maxH.size() - 1 == minH.size() ){
+	    minH.add(value);
+	} else if(maxH.size() > minH.size() + 1){
+	}
     }
 
     public double getMedian(){
-
-
+	if ( maxH.size() == minH.size() ){
+	    return ( maxH.peek() + minH.peek() )/2.0;
+	} else if( maxH.size() - 1 == minH.size() ){
+	    return maxH.peek();
+	} else if( minH.size() - 1 == maxH.size() ){
+	    return minH.peek();
+	}
+	return -1;
     }
+    
 
 
 }
