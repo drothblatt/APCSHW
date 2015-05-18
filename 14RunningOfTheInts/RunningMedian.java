e256140a52990990c5ee5e5bcaaeb27c172111c6
David Rothblatt <daroth13@gmail.com>
2015-05-17 20:08:37 -0400

    fixed/tested add(). added toString(). added name().

--- 14RunningOfTheInts/RunningMedian.java
+++ 14RunningOfTheInts/RunningMedian.java
@@ -9,27 +9,29 @@ public class RunningMedian{
     }
 
     public void add(int value){
-	if (maxH.size() == minH.size()){
-	    maxH.add(value);
-	    fixIt();
-	} else if( value > getMedian() ){
-	    minH.add(value);
-	} else if( value < getMedian() ){
+	if (maxH.size() > 0 || minH.size() > 0){ // if either has a value already
+	    double m = getMedian();
+	    if( value > m ){
+		minH.add(value);
+	    } else if( value < m ){
+		maxH.add(value);
+	    }
+	} else{ // if both heaps have a size of zero
 	    maxH.add(value);
 	}
 	fixIt();
     }
 
     public void fixIt(){
-	if( maxH.size() + 1 > minH.size() ){
+	if( maxH.size() - 1 > minH.size() ){
 	    minH.add(maxH.remove());
-	} else if ( minH.size() + 1 > maxH.size() ) {
+	} else if ( minH.size() - 1 > maxH.size() ) {
 	    maxH.add(minH.remove());
 	}
     }
 
     public double getMedian(){
-	if ( maxH.size() == minH.size() ){
+	if ( maxH.size() == minH.size() ){		
 	    return ( maxH.peek() + minH.peek() )/2.0;
 	} else if( maxH.size() - 1 == minH.size() ){
 	    return (double) maxH.peek();
@@ -39,23 +41,31 @@ public class RunningMedian{
 	return -1;
     }
 
+    public String toString(){
+	String retStr = ""; 
+	retStr += "\n" + "maxH: " + maxH + "\n";
+	retStr += "minH: " + minH + "\n";
+	retStr += "Median: " + getMedian() + "\n\n";
+	return retStr;
+    }
+
+    public String name(){
+	return "rothblatt.david";
+    }
+
     public static void main(String[]args){
 	RunningMedian r = new RunningMedian();
 	RunningMedian s = new RunningMedian();
 
 	for (int i = 1; i < 9; i++){
 	    r.add(i);
-	    System.out.println("maxH: " + r.maxH);
-	    System.out.println("minH: " + r.minH);
-	    System.out.println("Median: " + r.getMedian() + "...\n\n...");
+	    System.out.println(r.toString());
 	}
 	System.out.println(".....\n.....");
        
 	for (int i = 8; i > 0; i--){
 	    s.add(i);
-	    System.out.println("maxH: " + s.maxH);
-	    System.out.println("minH: " + s.minH);
-	    System.out.println("Median: " + s.getMedian() + "...\n\n...");
+	    System.out.println(s.toString());
 	}
     }
     
