ba1f8bfb0f73403314aa5ebc84835360bd5cc222
David Rothblatt <daroth13@gmail.com>
2015-05-20 00:34:38 -0400

    heapToSort() added

--- 15HeapSort/HeapSort.java
+++ 15HeapSort/HeapSort.java
@@ -2,17 +2,12 @@ import java.util.*;
 
 public class HeapSort{
 
+    // heapify
+
     private static void heapify(int[] a){
 	int size = a.length;
 	for (int i = a.length - 1; i >= 0; i--){
 	    moveDown(a, i, size);
-	}
-    }
-    
-    private static void heapToSort(int[] a){ // utilize heap to sort the array
-	for (int i = a.length - 1; i >= 0; i--){
-	    swap(a, 0, i); // swap a[0], the max num of the heap, with the end. 
-	    moveDown(a, 0, i); // only reorganize numbers up to i. 
 	}
     }
 
@@ -32,25 +27,34 @@ public class HeapSort{
 	}
     }
 
-    public static void heapsort(int[] a){
-	heapify(a); // part 1 of heapsort, altering array to be a psuedo-heap
-	heapToSort(a); // part 2 of heapsort, moving max vals to the back one at a time
+    // heapToSort
+    
+    private static void heapToSort(int[] a){ // utilize heap to sort the array
+	for (int i = a.length - 1; i >= 0; i--){
+	    swap(a, 0, i); // swap a[0], the max num of the heap, with the end. 
+	    moveDown(a, 0, i); // only reorganize numbers up to i. 
+	}
     }
-
 
     private static void swap(int[] a, int x, int y){
 	int temp = a[x];
 	a[x] = a[y];
 	a[y] = temp;
     }
-	       
+
+
+    // accessable, overarching sort method
+
+    public static void heapsort(int[] a){
+	heapify(a); // part 1 of heapsort, altering array to be a psuedo-heap
+	heapToSort(a); // part 2 of heapsort, moving max vals to the back one at a time
+    }
+
     public static void main(String[]args){
 	int[] x = {3, 9, 1, 6, 5, 8, 10, 2, 13, 4};
 	System.out.println(Arrays.toString(x));
 	heapsort(x);
 	System.out.println(Arrays.toString(x));
-
-
     }
 
 
