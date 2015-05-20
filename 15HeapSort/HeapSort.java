e8cab07953862333e09bf183492c964d1eb185bb
David Rothblatt <daroth13@gmail.com>
2015-05-20 00:16:17 -0400

    moveDown()

--- 15HeapSort/HeapSort.java
+++ 15HeapSort/HeapSort.java
@@ -3,18 +3,32 @@ import java.util.*;
 public class HeapSort{
 
     public static void heapify(int[] a){
+	int size = a.length;
 	for (int i = a.length - 1; i >= 0; i--){
-	    if (a[i] > a[i/2]){
-		swap(a, i, i/2);
-	    }
+	    moveDown(a, i, size);
 	}
 	System.out.println(Arrays.toString(a));
+
 	
     }
     
-    public static void fixHeap(int[] a){}
+    public static void moveDown(int[] a, int i, int size){
+	if ( ((i*2)+2) < size ){
+		if (a[i] < a[i*2]){
+		    swap(a, i, (i*2)+1);
+		    moveDown(a, (i*2)+1, size);
+		} 
+		if (a[i] < a[(i*2)+1] ){
+		    swap(a, i, (i*2)+2);
+		    moveDown(a, (i*2)+2, size);
+		}
+	} if ( ((i*2)+1) < size && a[i] < a[(i*2)+1] ){
+	    swap(a, i, (i*2)+1);
+	    moveDown(a, (i*2)+1, size);
+	}
+    }
 
-    public static void sort(int[] a){}
+    public static void heapsort(int[] a){}
 
 
     public static void swap(int[] a, int x, int y){
